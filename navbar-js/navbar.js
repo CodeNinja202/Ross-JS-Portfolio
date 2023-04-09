0afeaff5ea7e38e2aa023644ce377ac8130db4c3
Ross <rosskeene196@gmail.com>
2023-04-09 08:02:42 -0500

    saved

---
+++ navbar-js/navbar.js
@@ -0,0 +1,46 @@
+// Get references to the navigation links and the content div
+const homeLink = document.getElementById('home-link');
+const aboutLink = document.getElementById('about-link');
+const projectLink = document.getElementById('projects-link');
+const contactLink = document.getElementById('contact-link');
+const contentDiv = document.getElementById('content');
+
+// Define a function that loads the content of a page using AJAX
+function loadPage(url) {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      contentDiv.innerHTML = xhr.responseText;
+    }
+  };
+  xhr.open('GET', url);
+  xhr.send();
+}
+
+// Refreshed the page
+homeLink.addEventListener('click', function(event) {
+  location.reload();
+});
+
+// Add event listeners to the navigation links that load the content of each page
+aboutLink.addEventListener('click', function(event) {
+  event.preventDefault();
+  loadPage('./about/about.html');
+});
+
+// Add event listeners to the navigation links that load the content of each page
+projectLink.addEventListener('click', function(event) {
+  event.preventDefault();
+  loadPage('./projects/projects.html');
+});
+
+// Add event listeners to the navigation links that load the content of each page
+contactLink.addEventListener('click', function(event) {
+  event.preventDefault();
+  loadPage('./contact/contact.html');
+});
+
+
+
+
+
